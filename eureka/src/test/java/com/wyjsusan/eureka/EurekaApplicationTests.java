b56dff07dacfad09a48676780a2ba71f6887bdc6
wyjsusan <yujuewa@gmail.com>
2019-12-15 16:01:04 -0800

    Eureka server

---
+++ eureka/src/test/java/com/wyjsusan/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyjsusan.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
